7a632577af55860b01306525b7c483a4ca8113e8
KhajonpopC <70530196+KhajonpopC@users.noreply.github.com>
2020-09-21 11:07:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var solutionEle = document.body.querySelector(".solution");
+
+var number = 5;
+
+var inputnumber =Number(prompt)(
+"Input a valid number");
+
+If (isNaN(inputnumber)) {
+solution.innerHTML= Number(prompt)(
+"Input a valid number");
+}else{
+  newnumber= number+ inputnumber
+}
+
+
+var newnumber= number +inputnumber;
+ if (newnumber > 10) {
+   solution.innerHTML =
+     "Program is over";
+ }else{
+   var inputnumber =Number(prompt)(
+"Input a valid number"); 
+ }
